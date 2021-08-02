5be9a0e96d27b598237c78f58e0fc60b4905f5b4
Daniel Vallejo <danielsantiagovallejoo98@gmail.com>
2021-08-02 12:57:24 -0500

    cambio prueba

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Injectable()
+export class DbzService {
+    private _personajes:Personaje[]=[
+        {nombre:"ghhh",
+        poder:5000
+        },{nombre:"ghhh",
+        poder:5000
+        }
+      ]
+    get personajes(): Personaje[]{
+        return [...this._personajes];
+    }
+    constructor() {
+        
+    }
+
+    agregarPersonaje(personaje:Personaje){
+        this._personajes.push(personaje);
+    }
+}
